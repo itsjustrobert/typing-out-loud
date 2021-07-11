286fc2b9f4577fe2fc01621a6187955379d70f0f
itsjustrobert <robertlove99999@gmail.com>
2021-07-10 20:08:00 -0500

    shell complete

--- Blog Website/JS/main.js
+++ Blog Website/JS/main.js
@@ -13,11 +13,19 @@ $(document).ready(function(){
 $('.owl-carousel').owlCarousel({
     loop: true,
     autoplay: true,
-    autoplayTimeout:3000,
+    autoplayTimeout:4000,
     dots: false,
     nav:true,
     navText:[$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")]
    
 });
+// click to scroll down
+$('.move-up span').click(function(){
+$('html, body').animate({
+    scrollTop:0
+}, 1000)
 
+})
+// AOS instance
+AOS.init();
 });
