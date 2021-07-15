371137f99ca5d58d96c929db7db98c2506fd15bb
itsjustrobert <robertlove99999@gmail.com>
2021-07-15 02:44:05 -0500

    added some more controks to the tetris game

--- TETRIS/java.js
+++ TETRIS/java.js
@@ -75,6 +75,24 @@ function undraw(){
 // make the tetrimino move down every second
 timerId = setInterval(moveDown, 850);
 
+// assign functions to keyCodes
+function control(e){
+  if(e.keyCode === 37){
+    moveLeft()
+    
+  }
+  else if(e.keyCode === 38){
+    // rotate
+  }
+  else if(e.keyCode === 39){
+    moveRight()
+  }
+  else if(e.keyCode === 40){
+ moveDown()
+  }
+}
+
+document.addEventListener('keyup', control)
 // move down function
 function moveDown(){
     undraw();
@@ -96,17 +114,25 @@ draw()
 }
 
 // move the tetrimino left, unless it is at the edge or there is a blockage
-function moveLeft(){
-    undraw();
-    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+function moveLeft() {
+  undraw()
+  const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+  if(!isAtLeftEdge) currentPosition -=1
+  if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+    currentPosition +=1
+  }
+  draw()
 
-
-
-    if(!isAtLeft) currentPosition -=1 
-    if(current.some(index => squares[currentPosition].classList.contains('taken'))){
-        currentPosition +=1
-    }
-    draw()
+}
+//  move the tetrimino right unless it is at the edge or there is a blockage
+function moveRight(){
+  undraw()
+  const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+  if(!isAtRightEdge) currentPosition +=1
+  if(current.some(index => squares[currentPosition + index].classList.contains('taken'))){
+    currentPosition -=1
+}
+draw()
 }
 
 
