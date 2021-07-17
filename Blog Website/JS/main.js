500c6028f08671ab34cc2a12fde0817b613c3d63
itsjustrobert <robertlove99999@gmail.com>
2021-07-17 18:17:29 -0500

    back to normal

--- Blog Website/JS/main.js
+++ Blog Website/JS/main.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 
 
-    $nav=$('.nav');
-    $toggleCollapse=$('.toggle-collapse');
+     $nav=$('.nav');
+     $toggleCollapse=$('.toggle-collapse');
 
-    /**click event on toggle menu */ 
-    $toggleCollapse.click(function(){
-    $nav.toggleClass('collapse');
-    })
+     /**click event on toggle menu */ 
+     $toggleCollapse.click(function(){
+     $nav.toggleClass('collapse');
+     })
 
-//oil carousel for blog
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoplayTimeout:4000,
-    dots: false,
-    nav:true,
-    navText:[$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")]
+ //oil carousel for blog
+ $('.owl-carousel').owlCarousel({
+     loop: true,
+     autoplay: true,
+     autoplayTimeout:4000,
+     dots: false,
+     nav:true,
+     navText:[$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")]
    
-});
+ });
 // click to scroll down
-$('.move-up span').click(function(){
-$('html, body').animate({
-    scrollTop:0
-}, 1000)
+ $('.move-up span').click(function(){
+ $('html, body').animate({
+     scrollTop:0
+ }, 1000)
 
-})
+ })
 // AOS instance
 AOS.init();
 });
